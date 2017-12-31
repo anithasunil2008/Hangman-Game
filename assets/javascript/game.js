1ca72903479b4972d429d70890ed63ac03d35076
anithasunil2008 <anithasunil2008@gmail.com>
2017-12-30 22:21:33 -0500

    finished code' q q x clear help

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,100 +1,134 @@
-// Variables declaration section
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    't', 'u', 'v', 'w', 'x', 'y', 'z'
-];
+window.onload = function() {
 
-words = ["maine", "florida", "texas", "california", "arizona", "NewYork"];
-hints = ["'Is known for its rocky coastline'", "'Disney land'", "'A river that forms its border with Mexico'",
-    "'Hollywood entertainment industry'", "'Is best known for the Grand Canyon'", "'Statue of liberty'"
-];
-var index = Math.floor(Math.random() * words.length);
-var computerChoice = words[index];
-var computerChoiceLen = computerChoice.length;
-var guesses = [];
-var counter = 0;
-var lives = 5;
-var check;
-var placeHolder = document.getElementById('hold');
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'
+    ];
 
+    var words = ["maine", "florida", "texas", "california", "arizona", "newyork"];
 
-// Logging computer choice and its length - Delete after final commit
-console.log(index);
-console.log(computerChoice);
-console.log(computerChoiceLen);
+    var hints = ["'Is known for its rocky coastline'", "'Disney land'", "'A river that forms its border with Mexico'",
+        "'Hollywood entertainment industry'", "'Is best known for the Grand Canyon'", "'Statue of liberty'"
+    ];
 
-//Establish lives for the player
-var chances = document.getElementById('lives');
-chances.innerHTML = lives;
+    var flagSong = ["https://www.youtube.com/embed/xXknnb-d4WU",
+        "https://www.youtube.com/embed/uweoegT29Cs",
+        "https://youtu.be/bA1ZIyeY91E",
+        "https://youtu.be/jft8cQPgmi0",
+        "https://youtu.be/l-Wg_lDs2OY",
+        "https://youtu.be/ptdQUJEEpyM"
+    ];
 
-//Hints rendering logic
-hint.onclick = function() {
-    console.log(hints[index]);
-    document.getElementById('hintPrompt').innerHTML = hints[index];
+    var showLives = document.getElementById("mylives");
+    var showCatagory = document.getElementById("scatagory");
+    var getHint = document.getElementById("hint");
+    var showClue = document.getElementById("clue");
+    var letterGuess;
+    var getHint;
+    var guess;
+    var guesses = [];
+    var lives;
+    var counter;
+    var space;
+    var empty = [];
 
-}
+    var index = Math.floor(Math.random() * words.length);
+    console.log(index);
+    var computerChoice = words[index];
+    console.log(computerChoice);
 
-//Play Again logic
-var playAgain = document.getElementById('reset');
-playAgain.onclick = function() {
-    window.location.reload(true);
-}
+    var buttons = function() {
+        myButtons = document.getElementById('buttons');
+        letters = document.createElement('ul');
 
-//Game Over
-function gameOver() {
-    if (lives == 0) {
-        alert("Game Over");
-    }
-}
-
-//Rendering alphabet buttons
-function buttons() {
-    myButtons = document.getElementById('buttons');
-    letters = document.createElement('ul');
-    letters.id = 'letterList';
-
-    alphabet.forEach(e => {
-        var list = document.createElement('button');
-        list.className = 'letter';
-        list.innerHTML = e;
-        console.log(list.innerHTML);
-        letters.appendChild(list);
-    });
-
-    myButtons.appendChild(letters);
-}
-
-
-// Logic to replace dashes with the guessed letter if it is present in the word
-var guess = [];
-result = function() {
-
-}
-
-// Main game function ---- All support functions should be written above this function
-play = function() {
-    // Render letter buttons
-    buttons();
-
-    // Render the generated buttons into a new unordered list
-    placeHolderElement = document.createElement('ul');
-
-    // Loop to render the dashes based on the computer choice
-    for (i = 0; i < computerChoiceLen; i++) {
-        phList = document.createElement('li');
-        phList.className = 'dashes';
-        phList.innerHTML = '_';
-        placeHolderElement.appendChild(phList);
+        for (var i = 0; i < alphabet.length; i++) {
+            letters.id = 'alphabet';
+            list = document.createElement('li');
+            list.id = 'letter';
+            list.innerHTML = alphabet[i];
+            check();
+            myButtons.appendChild(letters);
+            letters.appendChild(list);
+        }
     }
 
-    // Attach the newly created unordered list to the answer placeholder
-    placeHolder.appendChild(placeHolderElement);
+    main = function() {
+        wordHolder = document.getElementById('hold');
+        correct = document.createElement('ul');
 
+        for (var i = 0; i < computerChoice.length; i++) {
+            correct.setAttribute('id', 'my-word');
+            guess = document.createElement('li');
+            guess.setAttribute('class', 'guess');
+            if (computerChoice[i] === "-") {
+                //  guess.innerHTML = "-";
+            } else {
+                guess.innerHTML = "_";
+            }
+            guesses.push(guess);
+            wordHolder.appendChild(correct);
+            correct.appendChild(guess);
+        }
+    }
+
+    comments = function() {
+        showLives.innerHTML = lives;
+        if (lives < 1) {
+            showLives.innerHTML = "Game Over";
+            document.getElementById('stateSong').src = flagSong[index];
+        }
+        for (var i = 0; i < guesses.length; i++) {
+            if (counter + space === guesses.length) {
+                showLives.innerHTML = "You Win!";
+            }
+        }
+    }
+
+    check = function() {
+        list.onclick = function() {
+
+            var g = document.getElementById("guessesLetter");
+
+            letterGuess = this.innerHTML;
+            console.log(letterGuess);
+            empty.push(letterGuess);
+            console.log(empty);
+            g.innerHTML = empty;
+
+            for (var i = 0; i < computerChoice.length; i++) {
+                if (computerChoice[i] === letterGuess) {
+                    guesses[i].innerHTML = letterGuess;
+                    counter += 1;
+                }
+            }
+            var j = (computerChoice.indexOf(letterGuess));
+            if (j === -1) {
+                lives -= 1;
+                comments();
+
+            } else {
+                comments();
+            }
+        }
+    }
+
+    play = function() {
+        buttons();
+        guesses = [];
+        lives = 10;
+        counter = 0;
+        main();
+        comments();
+    }
+
+    play();
+
+    hint.onclick = function() {
+        console.log(hints[index]);
+        document.getElementById('clue').innerHTML = hints[index];
+    }
+
+    document.getElementById('reset').onclick = function() {
+        window.location.reload(true);
+    }
 }
-
-document.getElementsByClassName('letter').onclick = function() {
-    console.log(this.innerHTML);
-}
-
-// Execute the main function on page load
-window.onload = play();
